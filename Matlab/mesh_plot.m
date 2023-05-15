73f8c6941aabef8861da998d4d5670dd4a5baf8a
nicogross <gross_nico@gmx.de>
2023-05-15 13:18:27 +0200

    added strcat

--- Matlab/mesh_plot.m
+++ Matlab/mesh_plot.m
@@ -1,11 +1,15 @@
 clear
 close
 
-nodes = readmatrix('../Problem/rectangle_3x3.co', 'FileType', 'text');
-Element = readmatrix('../Problem/rectangle_3x3.el', 'FileType', 'text');
-BD = readmatrix('../Problem/rectangle_3x3.bd', 'FileType', 'text');
+str = 'rectangle_1x1_refined2times';
+
+nodes = readmatrix(strcat('../Problem/',str,'.co'), 'FileType', 'text');
+Element = readmatrix(strcat('../Problem/',str,'.el'), 'FileType', 'text');
+BD = readmatrix(strcat('../Problem/',str,'.bd'), 'FileType', 'text');
 
 plot_mesh(nodes, Element, BD);
+
+%%
 
 nodes = readmatrix('../Problem/rectangle_3x3_refined.co', 'FileType', 'text');
 Element = readmatrix('../Problem/rectangle_3x3_refined.el', 'FileType', 'text');
