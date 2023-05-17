7194a669366200c667e0ef5f807651b0cd648cba
nicogross <gross_nico@gmx.de>
2023-05-17 18:02:17 +0200

    added matrix_print.c

--- Source/matrix_print.c
+++ Source/matrix_print.c
@@ -11,7 +11,7 @@ void print_matrix(index* A, index rows, index cols, bool colmajor, char* name, c
     for(int i=0; i<rows ; i++) {
         for(int j=0; j<cols; j++) {
             fprintf(f, "%zu", A[i*incRow+j*incCol]);
-            if(j==cols-1) {
+            if(j<cols-1) {
                 fprintf(f, "\t");
             }
         }
