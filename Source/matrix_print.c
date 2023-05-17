2cde2f3501bfe682432309bc52147604b9072d71
nicogross <gross_nico@gmx.de>
2023-05-17 17:58:37 +0200

    added matrix_print.c

---
+++ Source/matrix_print.c
@@ -0,0 +1,21 @@
+#include "hpc.h"
+
+void print_matrix(index* A, index rows, index cols, bool colmajor, char* name, char* extension) {
+    
+    index incRow = colmajor? cols : 1;
+    index incCol = colmajor? 1 : rows;
+ 
+    char filename[64]; sprintf(filename, "%s.%s", name, extension);
+
+    FILE* f = fopen(filename, "w");
+    for(int i=0; i<rows ; i++) {
+        for(int j=0; j<cols; j++) {
+            fprintf(f, "%zu", A[i*incRow+j*incCol]);
+            if(j==cols-1) {
+                fprintf(f, "\t");
+            }
+        }
+        fprintf(f,"\n");
+    }
+    fclose(f);
+}
