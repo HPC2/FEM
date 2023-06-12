443814de507a6564e02e1ebae65d4d66f2e14edc
Daniel Müller <acct.d-mueller@mailbox.org>
2023-06-12 17:46:01 +0200

    new runs

--- Demo/run_square_eq_dof.sh
+++ Demo/run_square_eq_dof.sh
@@ -44,6 +44,16 @@ make seq > /dev/null
 RESULT_NAME="main_square_eq_dof"
 HOSTFILE_NAME="hf_main_square_eq_dof"
 
+
+# 1 1 10; 2 2 9; 4 4 8; 8 8 7
+run_experiment 0 1 "./seq_fem 1 1 10 cg $RESULT_NAME"
+hostfile 4 0 0 0 0 0 0 0 $HOSTFILE_NAME
+run_experiment 0 1 "mpirun -np 4 --hostfile $HOSTFILE_NAME mpi_fem 2 2 9 cg $RESULT_NAME"
+hostfile 4 4 4 4 0 0 0 0 $HOSTFILE_NAME
+run_experiment 0 1 "mpirun -np 16 --hostfile $HOSTFILE_NAME mpi_fem 4 4 8 cg $RESULT_NAME"
+hostfile 4 4 4 4 4 4 4 4 $HOSTFILE_NAME
+run_experiment 0 1 "mpirun -np 64 --hostfile $HOSTFILE_NAME mpi_fem 8 8 7 cg $RESULT_NAME"
+
 # 1 1 12; 2 2 11; 4 4 10; 8 8 9
 run_experiment 0 1 "./seq_fem 1 1 12 cg $RESULT_NAME"
 hostfile 4 0 0 0 0 0 0 0 $HOSTFILE_NAME
@@ -61,15 +71,6 @@ hostfile 4 4 4 4 0 0 0 0 $HOSTFILE_NAME
 run_experiment 0 1 "mpirun -np 16 --hostfile $HOSTFILE_NAME mpi_fem 4 4 11 cg $RESULT_NAME"
 hostfile 4 4 4 4 4 4 4 4 $HOSTFILE_NAME
 run_experiment 0 1 "mpirun -np 64 --hostfile $HOSTFILE_NAME mpi_fem 8 8 10 cg $RESULT_NAME"
-
-# 1 1 10; 2 2 9; 4 4 8; 8 8 7
-run_experiment 0 1 "./seq_fem 1 1 12 cg $RESULT_NAME"
-hostfile 4 0 0 0 0 0 0 0 $HOSTFILE_NAME
-run_experiment 0 1 "mpirun -np 4 --hostfile $HOSTFILE_NAME mpi_fem 2 2 11 cg $RESULT_NAME"
-hostfile 4 4 4 4 0 0 0 0 $HOSTFILE_NAME
-run_experiment 0 1 "mpirun -np 16 --hostfile $HOSTFILE_NAME mpi_fem 4 4 10 cg $RESULT_NAME"
-hostfile 4 4 4 4 4 4 4 4 $HOSTFILE_NAME
-run_experiment 0 1 "mpirun -np 64 --hostfile $HOSTFILE_NAME mpi_fem 8 8 9 cg $RESULT_NAME"
 
 # 1 1 11; 2 2 10; 4 4 9; 8 8 8
 run_experiment 0 1 "./seq_fem 1 1 11 cg $RESULT_NAME"
