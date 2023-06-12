8f32e4a9a3eba094f63bc8f8a75d43041b72288d
marius_linux <marius.willner@uni-ulm.de>
2023-06-12 09:08:26 +0200

    send&recv to isend&irecv

--- Source/mpi_sum.c
+++ Source/mpi_sum.c
@@ -24,6 +24,8 @@ void mpi_sum_crosspoints(coupling_data* coupling, double* x, double* cp_buffer) 
 void mpi_sum_interfaces(coupling_data* coupling, double* x, double* if_buffer_send, double* if_buffer_recv) {
     int rank; MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     struct timeval t0; gettimeofday(&t0, (struct timezone*)0);
+    MPI_Request send_requests[4];
+    MPI_Request recv_requests[4];
     for(int i=0; i<4; i++) { // iterate through all 4 colors
         index left  = coupling->coupl_sorted[5*i+2];
         index right = coupling->coupl_sorted[5*i+3];
@@ -48,11 +50,12 @@ void mpi_sum_interfaces(coupling_data* coupling, double* x, double* if_buffer_se
         }
 
         if(rank == left) { // I am left and communicate with right
-            MPI_Send(if_buffer_send, n_nodes_interface, MPI_DOUBLE, color==-1? MPI_PROC_NULL : right, 0, MPI_COMM_WORLD);
-            MPI_Recv(if_buffer_recv, n_nodes_interface, MPI_DOUBLE, color==-1? MPI_PROC_NULL : right, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Irecv(if_buffer_recv, n_nodes_interface, MPI_DOUBLE, color==-1? MPI_PROC_NULL : right, 0, MPI_COMM_WORLD, &recv_requests[i]);
+            MPI_Isend(if_buffer_send, n_nodes_interface, MPI_DOUBLE, color==-1? MPI_PROC_NULL : right, 0, MPI_COMM_WORLD, &send_requests[i]);
+            
         }else{             // I am right and communicate with left
-            MPI_Recv(if_buffer_recv, n_nodes_interface, MPI_DOUBLE, color==-1? MPI_PROC_NULL : left, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(if_buffer_send, n_nodes_interface, MPI_DOUBLE, color==-1? MPI_PROC_NULL : left, 0, MPI_COMM_WORLD);
+            MPI_Irecv(if_buffer_recv, n_nodes_interface, MPI_DOUBLE, color==-1? MPI_PROC_NULL : left, 0, MPI_COMM_WORLD, &recv_requests[i]);
+            MPI_Isend(if_buffer_send, n_nodes_interface, MPI_DOUBLE, color==-1? MPI_PROC_NULL : left, 0, MPI_COMM_WORLD, &send_requests[i]);
         }
 
         if(color > -1) {
@@ -60,9 +63,15 @@ void mpi_sum_interfaces(coupling_data* coupling, double* x, double* if_buffer_se
                 x[if_nodes[j]] += if_buffer_recv[j]; // accumulating
             }
         }
-        struct timeval t1; gettimeofday(&t1, (struct timezone*)0);
-        if_comm += (size_t) (1.E+6*(t1.tv_sec-t0.tv_sec)+(t1.tv_usec-t0.tv_usec));
     }
+
+    for(int i = 0; i < 4; i++) {
+            MPI_Wait(&send_requests[i], MPI_STATUS_IGNORE);
+            MPI_Wait(&recv_requests[i], MPI_STATUS_IGNORE);
+        }
+
+    struct timeval t1; gettimeofday(&t1, (struct timezone*)0);
+    if_comm += (size_t) (1.E+6*(t1.tv_sec-t0.tv_sec)+(t1.tv_usec-t0.tv_usec));
 }
 
 void mpi_convert_type2_to_type1(coupling_data* coupling, double* x, comm_buffers* buffers) {
